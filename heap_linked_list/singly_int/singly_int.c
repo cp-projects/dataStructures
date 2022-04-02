7f5feff37a8ba3826eac0557e89fb44ff4336b3a
CPETER52 <cpeter52@depaul.edu>
2022-04-01 20:30:36 -0500

    make a real exit/error in place of, return -1, placeholder

--- heap_linked_list/singly_int/singly_int.c
+++ heap_linked_list/singly_int/singly_int.c
@@ -116,8 +116,9 @@ int dequeue(singly_t* list){
         break;
     }
     
+    //maybe make assert statement
     if(!(remove_node->val))
-        return -1;
+        exit(1);
 
     int val = *(remove_node->val);
 
@@ -138,54 +139,3 @@ int dequeue(singly_t* list){
     return val;
 }
 
-
-/*
-
-int main(){
-	
-singly_t* my_list = create_list();
-
-push(my_list, 1);
-push(my_list, 2);
-push(my_list, 3);
-push(my_list, 4);
-
-
-int one = pop(my_list);
-printf("The Popped Value is %d\n", one);
-
-int two = pop(my_list);
-printf("The Popped Value is %d\n", two);
-
-int three = pop(my_list);
-printf("The Popped Value is %d\n", three);
-
-int four = pop(my_list);
-printf("The Popped Value is %d\n", four);
-
-
-enqueue(my_list, 5);
-enqueue(my_list, 6);
-enqueue(my_list, 7);
-enqueue(my_list, 8);
-
-
-int five = dequeue(my_list);
-printf("The Dequeued Value is %d\n", five);
-
-int six = dequeue(my_list);
-printf("The Dequeued Value is %d\n", six);
-
-int seven = dequeue(my_list);
-printf("The Dequeued Value is %d\n", seven);
-
-int eight = dequeue(my_list);
-printf("The Dequeued Value is %d\n", eight);
-
-
-
-clear_list(my_list);
-
-return 0;
-}
-*/
