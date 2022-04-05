7947d92cdf8e33f1ab5099f21e2b2aad0e617644
CPETER52 <cpeter52@depaul.edu>
2022-04-04 19:16:27 -0500

    doubly linked list for type double works with equivalent functionality to singly linked list

--- heap_linked_list/doubly_linked/doubly_dbl/singly_dbl.c
+++ heap_linked_list/doubly_linked/doubly_dbl/singly_dbl.c
@@ -33,20 +33,20 @@ void clear_list(singly_t* list){
      singly_dbl_t* pos;
      pos = list->head;
 
-     singly_dbl_t* trace;
-     trace = list->head;
+     singly_dbl_t* swap;
+     swap = list->head;
 
      while(pos){
 
          if(pos->next){
-             trace = pos->next;      
+             swap = pos->next;      
 	     free(pos->val);
              pos->val = NULL;
 	     free(pos);
              
 	     --(list->len);
 	     
-	     pos = trace;
+	     pos = swap;
             }
 
          else{
@@ -63,17 +63,17 @@ void clear_list(singly_t* list){
 
 void push(singly_t* list, const double val){
 
+    singly_dbl_t* old_head = list->head;	
+	
     singly_dbl_t* new_node = (singly_dbl_t*) malloc(sizeof(singly_dbl_t));
     new_node -> val = (double*) malloc(sizeof(double));
     *(new_node->val) = val;
 
-    //list->head->prev = new_node;
     new_node->next = list->head;
-    //new_node->next->prev = new_node;
     list->head = new_node;
+    if(old_head)
+      old_head->prev = new_node;
     
-
-
     (list->len)++;
     return;
 }
@@ -96,12 +96,17 @@ double pop(singly_t* list){
 //(verify this later, but I can't see why it wouldn't be)
 void enqueue(singly_t* list, const double val){
     
+    singly_dbl_t* old_head = list->head;
+    
     singly_dbl_t* new_node = (singly_dbl_t*) malloc(sizeof(singly_dbl_t));
-
     new_node -> val = (double*) malloc(sizeof(double));
     *(new_node->val) = val;
+
     new_node->next = list->head;
     list->head = new_node;
+    if(old_head)
+      old_head->prev = new_node;
+
     (list->len)++;
     return;
 }
@@ -112,19 +117,13 @@ double dequeue(singly_t* list){
     singly_dbl_t* remove_node;
     remove_node = list->head;
 
-    singly_dbl_t* trace;
-    trace = list->head;
-
     while(remove_node){
-    if(remove_node->next){
-	trace = remove_node;    
+    if(remove_node->next)    
         remove_node = remove_node->next;
-       }
     else
         break;
     }
     
-    //maybe make assert statement
     if(!(remove_node->val))
         exit(1);
 
@@ -139,7 +138,7 @@ double dequeue(singly_t* list){
 	return val;
     }
     
-    trace->next = remove_node->next;
+    remove_node->prev->next = remove_node->next;
     free(remove_node->val);
     free(remove_node);
 
