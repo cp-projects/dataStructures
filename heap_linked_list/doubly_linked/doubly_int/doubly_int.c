0991416bdf46e0b5e026c7286d63a49485a6a26b
CPETER52 <cpeter52@depaul.edu>
2022-04-04 20:12:35 -0500

    got int working to the same level as double

--- heap_linked_list/doubly_linked/doubly_int/doubly_int.c
+++ heap_linked_list/doubly_linked/doubly_int/doubly_int.c
@@ -33,27 +33,29 @@ void clear_list(doubly_t* list){
      doubly_int_t* pos;
      pos = list->head;
 
-     //doubly_int_t* trace;
-     //trace = list->head;
+     doubly_int_t* swap;
+     swap = list->head;
 
      while(pos){
 
-         if(pos != list->head){
-             pos->prev->next = pos->next;      
-	     free(pos->val);
-	     free(pos);
-	     --(list->len); 
+         if(pos->next){
+             swap = pos->next;
+             free(pos->val);
+             pos->val = NULL;
+             free(pos);
+
+             --(list->len);
+
+             pos = swap;
             }
 
          else{
-	     free(pos->val);
+             free(pos->val);
              free(pos);
-	     pos = NULL;
-	     --(list->len);
+             pos = NULL;
             }
-	 pos = pos->next;
         } //end while
- 
+
     free((void*)list);
     return;
 }
@@ -61,16 +63,17 @@ void clear_list(doubly_t* list){
 
 void push(doubly_t* list, int val){
 
-    doubly_int_t* new_node = (doubly_int_t*) malloc(sizeof(doubly_int_t));
-    doubly_int_t* head_node = list->head;
+    doubly_int_t* old_head = list->head;
 
+    doubly_int_t* new_node = (doubly_int_t*) malloc(sizeof(doubly_int_t));
     new_node -> val = (int*) malloc(sizeof(int));
     *(new_node->val) = val;
 
-    //head_node->prev = new_node;
-    new_node->next = head_node;;
-    
+    new_node->next = list->head;
     list->head = new_node;
+    if(old_head)
+      old_head->prev = new_node;
+
     (list->len)++;
     return;
 }
@@ -79,12 +82,9 @@ int pop(doubly_t* list){
 
     doubly_int_t* remove_node;
     remove_node = list->head; 
-    
-    int val = *(remove_node->val);
 
-    
-    list->head = remove_node->next;   
-    
+    int val = *(remove_node->val);
+    list->head = remove_node->next;
     free(remove_node->val);
     free(remove_node);
 
@@ -96,18 +96,17 @@ int pop(doubly_t* list){
 //(verify this later, but I can't see why it wouldn't be)
 void enqueue(doubly_t* list, int val){
     
-    doubly_int_t* head_node = list->head;
-    doubly_int_t* new_node = (doubly_int_t*) malloc(sizeof(doubly_int_t));
-    
-    head_node->prev = new_node;
+    doubly_int_t* old_head = list->head;
 
+    doubly_int_t* new_node = (doubly_int_t*) malloc(sizeof(doubly_int_t));
     new_node -> val = (int*) malloc(sizeof(int));
     *(new_node->val) = val;
 
     new_node->next = list->head;
-    head_node->prev = new_node;
-
     list->head = new_node;
+    if(old_head)
+        old_head->prev = new_node;
+
     (list->len)++;
     return;
 }
@@ -118,14 +117,9 @@ int dequeue(doubly_t* list){
     doubly_int_t* remove_node;
     remove_node = list->head;
 
-    doubly_int_t* trace;
-    trace = list->head;
-
     while(remove_node){
-    if(remove_node->next){
-	trace = remove_node;    
+    if(remove_node->next)    
         remove_node = remove_node->next;
-       }
     else
         break;
     }
@@ -145,7 +139,7 @@ int dequeue(doubly_t* list){
 	return val;
     }
     
-    trace->next = remove_node->next;
+    remove_node->prev->next = remove_node->next;
     free(remove_node->val);
     free(remove_node);
 
