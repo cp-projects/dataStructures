a586273c8fccad842d06adbc28e9dfc81e2efb40
cp-projects <cpeter52@depaul.edu>
2022-04-22 04:47:52 -0500

    There is a seg fault but I am close to a full working bst implamentation

--- tier_2/tier_1/binary_trees/degenerate/right_degen/right_degen_dbl/right_degen_dbl.c
+++ tier_2/tier_1/binary_trees/degenerate/right_degen/right_degen_dbl/right_degen_dbl.c
@@ -1,33 +1,185 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "right_degen_dbl.h"
 
 
-typedef struct singly_dbl{
+typedef struct right_degen_bst_dbl{
     double* val;
-    singly_dbl_t *next;
-}singly_dbl_t;
+    right_degen_bst_dbl_t *right, *left, *prev;
+}right_degen_bst_dbl_t;
 
 
-typedef struct singly{
-    singly_dbl_t* head;
+typedef struct right_degen_bst{
+    right_degen_bst_dbl_t* root;
     size_t len;
-} singly_t;
+} right_degen_bst_t;
 
 
-singly_t* create_list(){
-    singly_t* list = (singly_t*) malloc(sizeof(singly_t));
-    list->len = 0;
-    list->head = NULL;
-    return list;        
+right_degen_bst_t* create_tree_r_dg_bst_dbl(){
+    right_degen_bst_t* tree = (right_degen_bst_t*) malloc(sizeof(right_degen_bst_t));
+    tree->len = 0;
+    tree->root = NULL;
+    return tree;        
 }
 
-size_t ret_len(singly_t* list){
-        return list->len;
+size_t ret_len_r_dg_bst_dbl(right_degen_bst_t* tree){
+        return tree->len;
 }
 
+
+void insert_r_dg_bst_dbl(right_degen_bst_t* tree, double val){
+
+   //create new node
+   right_degen_bst_dbl_t* new_node = (right_degen_bst_dbl_t*) malloc(sizeof(right_degen_bst_dbl_t));
+   new_node->val = (double*) malloc(sizeof(double));
+   *(new_node->val) = val;
+   
+   new_node->right = NULL;
+   new_node->left = NULL;
+
+   //if tree is  empty
+   if(!(tree->root)){
+       tree->root = new_node;
+       (tree->len)++;
+       return;
+   }
+
+   //if not empty, finding where to insert
+   right_degen_bst_dbl_t* pos = tree->root;
+
+   //breaking on the node to insert on
+   while(pos){
+	   
+       if(!(pos->right) || !(pos->left))
+           break; 
+       if(val < *(pos->val))
+	   pos = pos->left;
+       if(val >= *(pos->val))
+	   pos = pos->right;
+         }
+   
+   //put on the left if value is less
+   if(val < *(pos->val)){
+       new_node->prev = pos;
+       pos->left = new_node;
+       (tree->len)++;
+       return;
+      }
+
+    //put on the right if value is more
+    if(val > *(pos->val)){       
+       new_node->prev = pos;
+       pos->right = new_node;
+       (tree->len)++;
+       return;
+      }
+
+}
+
+
+
+void remove_r_dg_bst_dbl(right_degen_bst_t* tree, double val){
+
+    //don't try to remove an empty list
+    if(!(tree->root))
+       return;
+
+    //defining a position, trace, and parent_trace
+    right_degen_bst_dbl_t* pos = tree->root;
+    right_degen_bst_dbl_t* trace = tree->root;
+    right_degen_bst_dbl_t** parent_trace = &(tree->root);
+
+    //find the node to remove
+    while(pos){
+       
+       if(*(pos->val) == val)
+	   break;
+
+       else if(val < *(pos->val)){
+           trace = pos;
+	   parent_trace = &(pos->left);
+	   pos = pos->left;
+          }
+
+       else if(val >= *(pos->val)){
+	   trace = pos;
+           parent_trace = &(pos->right);
+           pos = pos->right;
+          }
+    }
+
+    //freeing that value
+    (tree->len)--;
+    free(pos->val);
+
+    //remove from right most node
+    if(pos->left == NULL){
+        *parent_trace = pos->right;
+	free(pos);
+	return;
+      }
+
+    //remove from left most node
+    if(pos->right == NULL){
+        *parent_trace = pos->left;
+	 free(pos);
+	 return;
+       }
+
+    //remove in the general case (this will degenerate to the right)
+    *parent_trace = pos->right;
+    right_degen_bst_dbl_t* leftIter = pos->right;
+
+    while(leftIter->left)
+	    leftIter = leftIter->left;
+
+    leftIter->left = pos->left;
+    *parent_trace = pos->right;
+    free(pos);
+
+    return;
+}
+
+
+static void clear_recursive_helper_r_dg_bst_dbl(right_degen_bst_dbl_t* node){
+    if(!node)
+        return;
+    clear_recursive_helper_r_dg_bst_dbl(node->left);
+    clear_recursive_helper_r_dg_bst_dbl(node->right);
+    free(node->val);
+    free(node);
+}
+
+void clear_r_dg_bst_dbl(right_degen_bst_t* tree){
+    right_degen_bst_dbl_t* pos = tree->root;
+    clear_recursive_helper_r_dg_bst_dbl(pos);
+    tree->root = NULL;
+    tree->len = 0;
+}
+
+
+void destroy_r_dg_bst_dbl(right_degen_bst_t* tree){
+    clear_r_dg_bst_dbl(tree);
+    free(tree); 
+}
+
+
+/*             FOR DEBUGGING
+int main(){
+
+    right_degen_bst_t* my_tree = create_tree_r_dg_bst_dbl();
+    insert_r_dg_bst_dbl(my_tree, 10);
+    insert_r_dg_bst_dbl(my_tree, 5);
+
+return 0;
+}
+*/
+
+
+/*       FOR INSPIRATION
 void clear_list(singly_t* list){
 
      singly_dbl_t* pos;
@@ -141,4 +293,4 @@ double dequeue(singly_t* list){
     (list->len)--;
     return val;
 
-}
+}*/
--- tier_2/tier_1/binary_trees/degenerate/right_degen/right_degen_dbl/right_degen_dbl.h
+++ tier_2/tier_1/binary_trees/degenerate/right_degen/right_degen_dbl/right_degen_dbl.h
@@ -1,11 +1,20 @@
 #ifndef rightDegenDbl_H_
 #define rightDegenDbl_H_
 
-typedef struct singly_dbl singly_dbl_t;
-typedef struct singly singly_t;
+typedef struct right_degen_bst_dbl right_degen_bst_dbl_t;
+typedef struct right_degen_bst right_degen_bst_t;
 
-singly_t* create_list();
-size_t ret_size(singly_t* list);
+right_degen_bst_t* create_tree_r_dg_bst_dbl();
+size_t ret_len_r_dg_bst_dbl(right_degen_bst_t* tree);
+
+void insert_r_dg_bst_dbl(right_degen_bst_t* tree, double val);
+void remove_r_dg_bst_dbl(right_degen_bst_t* my_tree, double val);
+
+static void clear_recursive_helper_r_dg_bst_dbl(right_degen_bst_dbl_t* node);
+void clear_r_dg_bst_dbl(right_degen_bst_t* tree);
+void destroy_r_dg_bst_dbl(right_degen_bst_t* tree);
+
+/*
 void clear_list(singly_t* list);
 
 void push(singly_t* list, double val);
@@ -13,6 +22,6 @@ double pop(singly_t* list);
 
 void enqueue(singly_t* list, double val);
 double dequeue(singly_t* list);
-
+*/
 
 #endif
